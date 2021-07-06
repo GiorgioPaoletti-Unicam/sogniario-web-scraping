7113389a3ba40656f62be7b177522c2183d17adc
Giorgio <giorgio.paoletti@studenti.unicam.it>
2021-07-06 18:41:29 +0000

    Implemented the spleepanddreamdatabaseSpider using Splash

--- dreamScaper/dreamScaper/settings.py
+++ dreamScaper/dreamScaper/settings.py
@@ -12,6 +12,23 @@ BOT_NAME = 'dreamScaper'
 SPIDER_MODULES = ['dreamScaper.spiders']
 NEWSPIDER_MODULE = 'dreamScaper.spiders'
 
+# Splash Setup
+
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'dreamScaper (+http://www.yourdomain.com)'
--- dreamScaper/dreamScaper/spiders/sleepanddreamdatabaseSpider.py
+++ dreamScaper/dreamScaper/spiders/sleepanddreamdatabaseSpider.py
@@ -1,21 +1,33 @@
 import scrapy
 from pymongo import MongoClient
+from scrapy_splash import SplashRequest
 
 
 class SleepanddreamdatabaseSpider(scrapy.Spider):
     name = 'sleepanddreamdatabaseSpider'
-    start_urls = ['https://sleepanddreamdatabase.org/search_results/falling_dreams']
+
+    def start_requests(self):
+        url = 'https://sleepanddreamdatabase.org/search_results/falling_dreams'
+
+        yield SplashRequest(url=url, callback=self.parse)
 
     def parse(self, response):
-
         client = MongoClient()
         db = client['Dreams']
         collection = db['sleepanddreamdatabaseDreams']
 
-        '''
-        for dream in response.css('p'):
-            if dream.css('span::text').get() is None:
-                continue
+        for dream in response.css('div.sc-pJVnX.jOExFg'):
+            dream_text = dream.css('div.sc-pjGMk.jrwOpx')
 
-            collection.insert_one({"text": dream.css('span::text').get()})
-        '''
+            dream_info = dream.css('div.sc-qZtVr.dLIPjF')
+
+            collection.insert_one(
+                {
+                    "text":
+                        dream_text.css('span::text').get(0) +
+                        dream_text.css('mark::text').get() +
+                        dream_text.css('span::text').get(1),
+                    "dreamerId": dream_info.css('div.sc-pzLqt.jakOsZ').get(0).css('div::text').get(1),
+                    "date": dream_info.css('div.sc-pzLqt.jakOsZ').get(1).css('div::text').get(1)
+                }
+            )
