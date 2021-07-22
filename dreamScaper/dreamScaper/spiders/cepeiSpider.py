f1de1ea2b6e9ad8d9add7445359e8de8a2c4bd49
Giorgio Paoletti <giorgio.paoletti@studenti.unicam.it>
2021-07-22 10:35:14 +0200

    Updated cepeiSpider
    Implemented algorithm that applies the Spider to all dream categories of the site

--- dreamScaper/dreamScaper/spiders/cepeiSpider.py
+++ dreamScaper/dreamScaper/spiders/cepeiSpider.py
@@ -2,27 +2,29 @@ import scrapy
 from pymongo import MongoClient
 
 
+def parse_link(response):
+
+    client = MongoClient()
+    db = client['Dreams']
+    collection = db['cepeiDreams']
+
+    for dream in response.css('p'):
+        if dream.css('span::text').get() is None:
+            continue
+
+        collection.insert_one({"text": dream.css('span::text').get()})
+
+
 class CepeiSpider(scrapy.Spider):
     name = 'cepeiSpider'
-    start_urls = ['https://www.cepei.it/morte/']
+
+    start_urls = ['https://www.cepei.it/i-sogni/']
 
     def parse(self, response):
 
-        client = MongoClient()
-        db = client['Dreams']
-        collection = db['cepeiDreams']
+        links = response.css('div.sogni')
 
-        for dream in response.css('p'):
-            if dream.css('span::text').get() is None:
-                continue
-
-            collection.insert_one({"text": dream.css('span::text').get()})
-
-            '''
-            yield {
-                'text': dream.css('span::text').get()
-            }
-            # scrapy crawl cepeiSpider -o dreams.json
-            '''
+        for url in links.css('a::text').getall():
+            yield scrapy.Request('https://www.cepei.it/' + url, parse_link)
 
 # scrapy crawl nameSpider
