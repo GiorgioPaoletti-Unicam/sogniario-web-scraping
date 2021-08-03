732676ce3f72b24cab675916db91f1f4a7646fe3
Giorgio Paoletti <giorgio.paoletti@studenti.unicam.it>
2021-08-03 14:42:23 +0200

    Implemented Splash Script for the website arhivsnova.hr

---
+++ dreamScaper/dreamScaper/spiders/arhivsnova_hrSplashScript.lua
@@ -0,0 +1,47 @@
+local treat = require('treat')
+
+function main(splash, args)
+    assert(splash:go(args.url))
+    splash:set_viewport_full()
+
+    local scroll_delay = 0.1
+    local num_scrolls = 100
+    local scroll_to = splash:jsfunc("window.scrollTo")
+
+    local get_body_height = splash:jsfunc(
+        "function() {return document.body.scrollHeight;}"
+    )
+
+    for _ = 1, num_scrolls do
+        scroll_to(0, get_body_height())
+        splash:wait(scroll_delay)
+    end
+
+
+  	local dreams = {}
+
+    local js_container = splash:select_all('ul#dreams a')
+
+  	for _, link in treat.as_array(js_container) do
+    	splash:runjs(link.node.attributes.href)
+
+        --[[
+        local info_container = splash:select('div#popup-inner p')
+
+        local text_container = treat.as_array(info_container)[2]
+
+        dreams.newKey = text_container:text()
+        --]]
+
+        local text_container = splash:select('div#popup-inner .dream-excerpt p')
+        dreams.newKey = text_container:text()
+
+        --dreams.newKey = treat.as_array(info_container)[2]:text()
+
+    end
+
+
+
+    --return splash:html()
+    return treat.as_array(dreams)
+end
