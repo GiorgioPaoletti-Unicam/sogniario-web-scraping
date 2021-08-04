c7664c5b6a6c8bba978ebfb9abfdacdac083bba3
Giorgio Paoletti <giorgio.paoletti@studenti.unicam.it>
2021-08-04 23:23:15 +0200

    Implemented dreamAnalyzer script

---
+++ dreamAnalyzer/main.py
@@ -0,0 +1,59 @@
+import spacy
+from pymongo import MongoClient
+
+if __name__ == '__main__':
+    client = MongoClient()
+    db = client['Dreams']
+
+    collections = [
+        "internationalarchiveofdreamsDreams",
+        "thedreamarchiveDreams",
+        # "cepeiDreams"
+    ]
+    collection_to = db["tokenizedDreams"]
+
+    nlp = None
+
+    for collection_name in collections:
+        for dream in db[collection_name].find({}):
+
+            try:
+                nlp = spacy.load(dream["language"] + "_core_web_sm")
+            except Exception:
+                print("The dream with the following Id: %s has a non-existent language" % dream["_id"])
+                continue
+
+            doc = nlp(dream["text"])
+
+            doc_map = {}
+            words = []
+            arcs = []
+            for token in doc:
+
+                token_text = token.text
+
+                if token.pos_ == "VERB":
+                    token_text = token.lemma_
+
+                if token.pos_ == "AUX":
+                    continue
+
+                if token.pos_ == "PART" and token.dep_ == "neg":
+                    token_text = "not"
+
+                words.append({
+                    "text": token_text,
+                    "pos": token.pos_
+                })
+
+                arcs.append({
+                    "start": token.head.i,
+                    "end": token.i,
+                    "label": token.dep_,
+                    "dir": "left" if token.head.i > token.i else "right"
+                })
+
+            doc_map["words"] = words
+            doc_map["arcs"] = arcs
+
+            collection_to.insert_one(doc_map)
